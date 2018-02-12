ad75f96566e2b5d971b5991774ce42b04e24be55
LibreDMM <admin@libredmm.com>
2018-02-11 20:25:16 -0800

    rebuild movie#index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.order(code: :asc).all
   end
 
   # GET /movies/1
