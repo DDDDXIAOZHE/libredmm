5f18d231aabc878f069786535ac9a09d3a3c4c63
LibreDMM <admin@libredmm.com>
2018-02-13 12:49:36 -0800

    add field is_admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213041827) do
+ActiveRecord::Schema.define(version: 20180213204713) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'movies', force: :cascade do |t|
-    t.string 'actresses', array: true
-    t.string 'actress_types', array: true
-    t.string 'categories', array: true
-    t.string 'code'
-    t.string 'cover_image'
-    t.string 'description'
-    t.string 'directors', array: true
-    t.string 'genres', array: true
-    t.string 'label'
-    t.string 'maker'
-    t.string 'movie_length'
-    t.string 'page'
-    t.string 'release_date'
-    t.string 'sample_images', array: true
-    t.string 'series'
-    t.string 'tags', array: true
-    t.string 'thumbnail_image'
-    t.string 'title'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "movies", force: :cascade do |t|
+    t.string "actresses", array: true
+    t.string "actress_types", array: true
+    t.string "categories", array: true
+    t.string "code"
+    t.string "cover_image"
+    t.string "description"
+    t.string "directors", array: true
+    t.string "genres", array: true
+    t.string "label"
+    t.string "maker"
+    t.string "movie_length"
+    t.string "page"
+    t.string "release_date"
+    t.string "sample_images", array: true
+    t.string "series"
+    t.string "tags", array: true
+    t.string "thumbnail_image"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email', null: false
-    t.string 'encrypted_password', limit: 128, null: false
-    t.string 'confirmation_token', limit: 128
-    t.string 'remember_token', limit: 128, null: false
-    t.index ['email'], name: 'index_users_on_email'
-    t.index ['remember_token'], name: 'index_users_on_remember_token'
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.boolean "is_admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
+
 end
