a6cf831e920392c191e18357a92da36f9115df9f
LibreDMM <admin@libredmm.com>
2018-03-06 10:33:46 -0800

    do not fail on update failure during migration

--- db/migrate/20180306064241_change_release_date_to_be_date_in_movies.rb
+++ db/migrate/20180306064241_change_release_date_to_be_date_in_movies.rb
@@ -3,8 +3,7 @@ class ChangeReleaseDateToBeDateInMovies < ActiveRecord::Migration[5.1]
     rename_column :movies, :release_date, :release_date_str
     add_column :movies, :release_date, :date
     Movie.find_each do |movie|
-      movie.update!(release_date: movie.release_date_str)
-      puts "#{movie.code}: #{movie.release_date_str} -> #{movie.release_date}"
+      movie.update(release_date: movie.release_date_str)
     end
     remove_column :movies, :release_date_str
   end
@@ -14,7 +13,6 @@ class ChangeReleaseDateToBeDateInMovies < ActiveRecord::Migration[5.1]
     add_column :movies, :release_date, :string
     Movie.find_each do |movie|
       movie.update!(release_date: movie.release_date_dt.to_s(:db))
-      puts "#{movie.code}: #{movie.release_date_dt} -> #{movie.release_date}"
     end
     remove_column :movies, :release_date_dt
   end
