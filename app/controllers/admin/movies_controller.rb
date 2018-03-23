cb7af71b704055ec6c5180e3f880717cf78c6238
LibreDMM <admin@libredmm.com>
2018-03-22 22:44:16 -0700

    administrate: support updating string array

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -17,5 +17,16 @@ module Admin
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
+
+    def resource_params
+      arr_fields = %i(actresses actress_types categories directors genres sample_images tags)
+      arr_fields.each do |field|
+        params[resource_name.to_s][field.to_s] = params[resource_name.to_s][field.to_s].split(' ')
+      end
+      params.require(resource_name).permit(
+        *(dashboard.permitted_attributes - arr_fields),
+        **(arr_fields.map { |field| [field, []] }.to_h)
+      )
+    end
   end
 end
